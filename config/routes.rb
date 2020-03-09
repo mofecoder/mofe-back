39474e411fec543c5b0d19b5a3c5331bd7105db2
sarashin <sarashina.nikki65@gmail.com>
2020-03-09 19:22:33 +0900

    とりあえずルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     resources :contests, param: :slug, only: [:index, :show] do
-      resources :tasks, param: :slug, only: [:show]
+      resource :submits, only: [:show] do
+        resource :all, only: [:show]
+      end
+      resources :tasks, param: :slug, only: [:show] do
+        resource :submit, only: [:create]
+      end
     end
   end
 end
