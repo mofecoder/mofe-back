4196bf643a56e0f7374923981eb31a380275e339
Takayuki Ueno <mail@kichi2004.jp>
2020-03-10 23:36:27 +0900

    ユーザ名でも認証できるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,16 +16,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-          authentication_keys: [:email]
+          authentication_keys: [:email, :name]
   include DeviseTokenAuth::Concerns::User
 
   validates :name, presence: true, uniqueness: { case_sensitive: true }
   validates_format_of :name, with: /\A[a-zA-Z0-9_]{3,12}\z/, multiline: false
-
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    p 'find called'
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
-  end
 end
