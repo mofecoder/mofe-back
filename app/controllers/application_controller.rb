2b11bc064bcc796e900444103340eea9d6279a95
Takayuki Ueno <mail@kichi2004.jp>
2020-03-10 20:40:25 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::API
+  rescue_from Exception, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
 
   def render_404(e = nil)
-    render json: e, status: 404
+    render json: { status: 404, error: e }, status: 404
   end
 
   def render_500(e = nil)
-    render json: e, status: 500
+    render json: { status: 500, error: e }, status: 500
   end
 end
