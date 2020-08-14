3d2575d2fdd4fa271cf2ceea519c44c925143377
Takayuki Ueno <mail@kichi2004.jp>
2020-08-14 22:14:48 +0900

    テストケースごとの結果の追加

--- app/serializers/submit_detail_serializer.rb
+++ app/serializers/submit_detail_serializer.rb
@@ -1,6 +1,11 @@
 class SubmitDetailSerializer < SubmitSerializer
-  attributes :source
+  attributes :source, :testcase_results
+
   def source
     Utils::GoogleCloudStorageClient::get_source(object.path).read
+  end
+
+  def testcase_results
+    object.testcase_results
   end
 end
---
+++ app/serializers/testcase_result_serializer.rb
@@ -0,0 +1,7 @@
+class TestcaseResultSerializer < ActiveModel::Serializer
+  attributes :testcase_name, :status, :execution_time, :execution_mempry
+
+  def testcase_name
+    object.testcase.name
+  end
+end
