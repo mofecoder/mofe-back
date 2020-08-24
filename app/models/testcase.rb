9541f4c07c4e7539a059e9a1a9239f1453447946
Takayuki Ueno <mail@kichi2004.jp>
2020-08-24 19:46:09 +0900

    fix

--- app/models/testcase.rb
+++ app/models/testcase.rb
@@ -1,5 +1,5 @@
 class Testcase < ApplicationRecord
   acts_as_paranoid
-  has_many :testcase_testcase_sets
-  has_many :testcase_sets, through: :testcase_testcase_sets, dependent: destroy
+  has_many :testcase_testcase_sets, dependent: :destroy
+  has_many :testcase_sets, through: :testcase_testcase_sets
 end
