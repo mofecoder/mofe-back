e62f1135862832c8f72e4242357397568656fb6e
Takayuki Ueno <mail@kichi2004.jp>
2020-09-15 20:56:29 +0900

    テスター関連の改善

--- app/controllers/api/contests_controller.rb
+++ app/controllers/api/contests_controller.rb
@@ -11,7 +11,8 @@ class Api::ContestsController < ApplicationController
   def show
     contest = Contest.includes(problems: :testcase_sets).find_by!(slug: params[:slug])
     include_tasks = contest.start_at.past? || (user_signed_in? && current_user.admin?)
-    render json: contest, serializer: ContestDetailSerializer, include_tasks: include_tasks
+    render json: contest, serializer: ContestDetailSerializer,
+           include_tasks: include_tasks, user: current_user
   end
 
   def create
