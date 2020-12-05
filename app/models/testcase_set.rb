7bb3a7f0b7b100d9817fe247b96b9106229f5a9f
kichi2004 <mail@kichi2004.jp>
2020-12-05 16:58:21 +0900

    テストケースセットが削除できない問題を修正

--- app/models/testcase_set.rb
+++ app/models/testcase_set.rb
@@ -1,5 +1,4 @@
 class TestcaseSet < ApplicationRecord
   has_many :testcase_testcase_sets, dependent: :destroy
   has_many :testcases, through: :testcase_testcase_sets
-  has_many :testcase_results, dependent: :destroy
 end
