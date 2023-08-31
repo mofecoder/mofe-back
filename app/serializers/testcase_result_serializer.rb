79c464f9709f955f0ef201a4ae4901eecdd72947
Takayuki Ueno <mail@kichi2004.jp>
2023-08-31 22:07:59 +0900

    fix: コンテスト中はテストケース一覧を表示しない

--- app/serializers/testcase_result_serializer.rb
+++ app/serializers/testcase_result_serializer.rb
@@ -1,4 +1,4 @@
-class TestcaseResultSerializer < HiddenTestcaseResultSerializer
+class TestcaseResultSerializer < ActiveModel::Serializer
   attributes :testcase_name, :status, :execution_time, :execution_memory
   def testcase_name
     object.testcase.name
