927427cb598ed28afde6666dc0ed73e6968509f4
kichi2004 <mail@kichi2004.jp>
2024-11-05 18:48:55 +0900

    Fix creating contest

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,7 +1,4 @@
 class Contest < ApplicationRecord
-  include ActiveModel::Model
-  include ActiveModel::Serialization
-
   has_many :problems, -> { order('char_length(`position`)').order(:position) }
   has_many :clarifications
   has_many :registrations, dependent: :destroy
