6c92d65858f61acb0cfff29a76b155395ddd3aa4
kichi2004 <mail@kichi2004.jp>
2024-11-08 22:32:41 +0900

    Fix submission list

--- app/controllers/api/submissions_controller.rb
+++ app/controllers/api/submissions_controller.rb
@@ -59,7 +59,10 @@ class Api::SubmissionsController < ApplicationController
                         .joins(problem: :contest)
                         .where("contests.slug = ?", contest_slug)
                         .where(problem_id: including_problem_id)
-                        .where("submissions.public OR submissions.user_id = ? OR ?", user_signed_in? ? current_user.id : -1, permission)
+                        .where("submissions.public OR submissions.user_id = ? OR problems.writer_user_id = ? OR ?",
+                               user_signed_in? ? current_user.id : -1,
+                               user_signed_in? ? current_user.id : -1,
+                               permission)
                         .page(page)
                         .per(count)
 
