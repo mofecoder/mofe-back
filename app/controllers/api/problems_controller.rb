b6dc81d9a25a72bff62db47fe463e34a7f3d6fa3
Takayuki Ueno <mail@kichi2004.jp>
2025-03-01 16:46:37 +0900

    Fix permission for problems#show

--- app/controllers/api/problems_controller.rb
+++ app/controllers/api/problems_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProblemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_writer!, only: [:index, :show, :create]
+  before_action :authenticate_writer!, only: [:index, :create]
 
   # GET /problems
   def index
